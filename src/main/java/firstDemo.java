a33ca74f7638220524e4ba7eb82bdb431535c327
huaqiliang <404281510@qq.com>
2020-11-05 22:20:05 +0800

    芜湖，起飞~

---
+++ src/main/java/firstDemo.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author huaqiliang
+ * @date: 2020/11/522:05
+ * @Description:helloW&M
+ * @Version:
+ */
+
+public class firstDemo {
+    public static void main(String[] args) {
+        System.out.println("hello ! 小孟同学");
+    }
+}
---
+++ src/main/java/playDemo.java
@@ -0,0 +1,9 @@
+/**
+ * @author huaqiliang
+ * @date: 2020/11/522:16
+ * @Description:写一些不同的代码
+ * @Version:
+ */
+
+public class playDemo {
+}
