4d0639e1547f31b585136a2e1513581af48e5972
huaqiliang <404281510@qq.com>
2020-11-09 23:59:44 +0800

    芜湖，起飞~

--- src/main/java/playDemo.java
+++ src/main/java/playDemo.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.Properties;
+
 /**
  * @author huaqiliang
  * @date: 2020/11/522:16
@@ -6,4 +9,20 @@
  */
 
 public class playDemo {
+    private final static Properties properties = new Properties();
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(System.getProperty("user.dir"));
+            FileInputStream fileInputStream = new FileInputStream(System.getProperty("user.dir") + File.separator + "src/main/resource/hql.system.properties");
+            Reader reader = new InputStreamReader(fileInputStream);
+            properties.load(reader);
+            System.out.println(properties.get("adress"));
+            System.out.println(properties.get("name"));
+            //System.out.println(System.getProperty("user.dir"));
+            //BufferedReader br = new BufferedReader(fr);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
